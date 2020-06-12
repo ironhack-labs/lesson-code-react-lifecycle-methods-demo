8d13df96556c2418f129a5c09cea7e46310962c3
sandrabosk <sandraboskovic@hotmail.com>
2020-06-12 18:12:12 -0400

    add lesson code example for lifecycle methods

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Counter from './components/Counter';
+
+class App extends React.Component {
+  state = {
+    count: 0,
+    counterIsActive: false
+  };
+
+  handleCountChange = value => {
+    this.setState({
+      count: value
+    });
+  };
+
+  toggleCounter = () => {
+    this.setState(previousState => ({
+      counterIsActive: !previousState.counterIsActive
+    }));
+  };
+
+  componentDidCatch(error) {
+    console.log('There was an uncaught error thrown in the component');
+    console.log('Error! ', error);
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <button onClick={this.toggleCounter}>Toggle</button>
+        {this.state.counterIsActive && <Counter value={this.state.count} onCountChange={this.handleCountChange} />}
+      </div>
+    );
+  }
 }
 
 export default App;
